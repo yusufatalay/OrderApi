ddb8c495ab8421e04eb90368d2417c5b44e73e69
yusufatalay <yusuftahaatalay14@gmail.com>
2022-03-17 10:05:59 +0300

    Installed and initialized sequelizer

---
+++ api/src/config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+	'development': {
+		'username': 'postgres',
+		'password': 'Killer14',
+		'database': 'gomovies',
+		'host': '127.0.0.1',
+		'dialect': 'postgres'
+	},
+	'test': {
+		'username': 'postgres',
+		'password': 'Killer14',
+		'database': 'gomovies',
+		'host': '127.0.0.1',
+		'dialect': 'postgres'
+	},
+	'production': {
+		'username': 'postgres',
+		'password': 'Killer14',
+		'database': 'gomovies',
+		'host': '127.0.0.1',
+		'dialect': 'postgres'
+	}
+};
